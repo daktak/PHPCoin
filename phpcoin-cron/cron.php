1186ce163c11a715b4d8c3364b29c5374a9d15c6
daktak <daktaklakpak@eml.cc>
2013-12-16 09:15:18 +1100

    fix

--- phpcoin-cron/cron.php
+++ phpcoin-cron/cron.php
@@ -29,9 +29,7 @@
       $q = mysqli_query($GLOBALS["___mysqli_ston"], $sql);
       if(!mysqli_num_rows($q)) continue; //Account not found
       $act = mysqli_fetch_assoc($q);
-      if ($config['central_account']['value'] > 0) {
-          $b[$x]->move($k,$config['central_account']['value'],$a);
-      }
+      $b[$x]->move($k,$config['central_account']['value'],$a);
       $prevBal = 0;
       $sql = "SELECT balance FROM movements WHERE account_id = {$act['id']} ORDER BY id DESC LIMIT 0,1";
       $q = mysqli_query($GLOBALS["___mysqli_ston"], $sql);
